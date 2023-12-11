5aeebafc0287829a37a403c6d4b0f8799905a99e
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2023-12-11 19:31:37 +0530

    Question 1

--- Ques1.js
+++ Ques1.js
@@ -1,5 +1,10 @@
 var str = "Hii i am a MERN Stack developer";
 
 var result = str.split(" ")
+    .map(word => word.split("").reverse().join("")
+    )
+//if we use split with nothing it will give us separate words like hii.split("")=>[i,i,h]
+//if we use split with space(" ") it will give us words like hii i am .split("")=>[hii,i]
 
-console.log(result)
+
+console.log(result.join(" "))
