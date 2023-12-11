a98ea29758f34d4e772030f921d7ab97cd96a4af
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2023-12-11 20:04:14 +0530

    Question 4

---
+++ Ques4.js
@@ -0,0 +1,16 @@
+//Check whether the number is an integer
+var a = 12.0;
+// if (a % 1 == 0) {
+//     console.log("Integer")
+// }
+// else {
+//     console.log("Not Integer")
+// }
+
+if (Number.isInteger(a)) {
+    console.log("Integer")
+
+} else {
+    console.log("Not Integer")
+
+}
