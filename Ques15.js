b1a2bbfaad48a3715f93c84040cf145b397959a9
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2023-12-17 22:42:12 +0530

    Ques15

---
+++ Ques15.js
@@ -0,0 +1,11 @@
+//Write a js function which returns the type
+
+const ArgType = (args) => {
+    console.log(typeof args)
+}
+ArgType({})
+ArgType(true)
+ArgType(undefined)
+ArgType(2)
+ArgType("djjds")
+ArgType(function () { })
